eecf36f64e5775309549c94a1cb92ce02e7eda71
Kimberly Bonon <officialberlcontact@gmail.com>
2025-06-17 18:09:46 +1300

    Initial commit for Egg Timer Project

---
+++ src/eggtimer/EggTimer.java
@@ -0,0 +1,12 @@
+package eggtimer;
+
+public class EggTimer {
+
+    public static void main(String[] args) {
+        EggTimerMain etm = new EggTimerMain();
+        etm.setVisible(true);
+        etm.pack();
+        etm.setLocationRelativeTo(null);
+    }
+    
+}
